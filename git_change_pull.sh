5c29a998b74e4bd71a49e3ef3c2165693d85000a
luojie <luojie@cncr-it.com>
2020-09-03 14:05:12 +0800

    修改shell脚本，增加函数

--- git_change_pull.sh
+++ git_change_pull.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+#子目录名
+subdirname1=git
+subdirname2=gitlab_external
+subdirname3=github
 
 project_path=$(cd `dirname $0`; pwd)
 project_name="${project_path##*/}"
 echo $project_path
 echo $project_name
+
 #建立缓存文件
 #touch templefile
-#函数
+
+#pull函数
 pull_branch(){
         cd $line
         branch=$(git branch -l | grep "^*")
@@ -25,7 +31,7 @@ do
 
     echo "进入目录$line"
     cd $line
-    if [ $line == git ]
+    if [ $line == $subdirname1 ] || [ $line == $subdirname2 ] || [ $line == $subdirname3 ]
     then
         dir2=$(ls -l $pwd |awk '/^d/ {print $NF}')
         for line in $dir2
@@ -45,14 +51,4 @@ do
     cd ..
 done
 
-pull_branch(){
-        cd $line
-        branch=$(git branch -l | grep "^*")
-        branch_name="${branch:2}"
-        git remote -v
-        git pull origin $branch_name
-        echo $line
-        echo "退出当前目录$line"
-        cd ..
-}
-
+#结束---------------------------------------------------------------------
--- git_change_push.sh
+++ git_change_push.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
+#子目录名
+subdirname1=git
+subdirname2=gitlab_external
+subdirname3=github
+
 project_path=$(cd `dirname $0`; pwd)
 project_name="${project_path##*/}"
-#echo $project_path
-#echo $project_name
+echo $project_path
+echo $project_name
+
+
+#push函数
+push_branch(){
+        branch=$(git branch -l | grep "^*")
+        branch_name="${branch:2}"
+        git remote -v
+        git push origin $branch_name
+        #echo $line
+        echo "退出当前目录$line"
+    }
 #列出当前文件夹所有目录
+
 dir=$(ls -l $pwd |awk '/^d/ {print $NF}')
 for line in $dir
 do
-
-
     echo "进入目录$line"
     cd $line
-    branch=$(git branch -l | grep "^*")
-    branch_name="${branch:2}"
-    git remote -v
-    git push origin $branch_name
-    echo $line
-    echo "退出当前目录$pwd"
+    #判断当前文件夹是否为git 子目录
+    if [ $line == $subdirname1 ] || [ $line == $subdirname2 ] || [ $line == $subdirname3 ]
+    then
+        dir2=$(ls -l $pwd |awk '/^d/ {print $NF}')
+        for line in $dir2
+            do
+                cd $line
+                push_branch
+                cd ..
+            done
+    else
+        push_branch
+        echo "退出当前目录$pwd"
+    fi
     cd ..
 done
